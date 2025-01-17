f8b7b24cb43625493dc978856a3e43f4b4439890
unknown <nipcts3@gmail.com>
2025-01-17 08:12:27 +0200

    created the contract

--- first_contract/contracts/Bucky.sol
+++ first_contract/contracts/Bucky.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.7;
+pragma solidity >=0.8.26;
 contract Bucky {
     uint age;
 
     function setAge(uint x) public {
         age = x;
     }
+    // the view is for fucntions that don't modify anything
     function getAge() public view returns (uint){
         return age;
     }
