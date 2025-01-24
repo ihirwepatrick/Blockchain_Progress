1d86dbe7882f3d9b02e1b37d883914c4c5dd16bf
unknown <nipcts3@gmail.com>
2025-01-24 09:07:35 +0200

    added functions

--- first_contract/contracts/integersTesting.sol
+++ first_contract/contracts/integersTesting.sol
@@ -7,5 +7,8 @@ contract unsignedIntExample {
     uint public defaultUint = 100; // default (256)
     uint8 public smallIntExample = 255; //store values ranging from 0-255
     uint16 public mediumIntExample = 65535; // stores values rangong from 0 - 65535
+    function addInts(uint _int1, uint _int2) public pure returns (uint) {
+        return _int1 + _int2;
+    }
 
 }
